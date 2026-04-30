9e9d1563091aae3d1e0ad95dfabe782bcbeefbf1
MSuzdalev <masksuzdalev@mail.ru>
2026-04-30 11:51:36 +0500

    Убраны репозитории

--- ApplicationModelNet/Domain/Repository/Reference/IReferenceRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Promatis.Net.Domain;
-
-public interface IReferenceRepository<T> : IBaseRepository<T, Guid> where T : ReferenceBase
-{
-    Task<T?> GetByCodeAsync(string code);
-
-    Task<List<T>> SearchByNameAsync(string namePart);
-}
