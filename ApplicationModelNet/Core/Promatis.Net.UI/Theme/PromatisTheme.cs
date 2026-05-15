45dcf9268bf213982f262c845baff2d9f07da695
MSuzdalev <masksuzdalev@mail.ru>
2026-05-15 15:59:03 +0500

    Зачатки UI

---
+++ ApplicationModelNet/Core/Promatis.Net.UI/Theme/PromatisTheme.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace Promatis.Net.Ui.Theme;
+
+public static class PromatisTheme
+{
+    public static MudTheme DefaultTheme => new()
+    {
+        PaletteLight = new PaletteLight
+        {
+            Primary = Colors.Blue.Lighten1,
+            Secondary = Colors.Blue.Lighten2,
+            AppbarBackground = Colors.Red.Default,
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = Colors.Blue.Lighten1
+        },
+        LayoutProperties = new LayoutProperties()
+        {
+            DrawerWidthLeft = "260px",
+            DrawerWidthRight = "300px"
+        }
+    };
+}
